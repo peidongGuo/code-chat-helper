a2a981446af8f408c2e17892533960aaadf00644
LI-Mingyu <limingyu@ict.ac.cn>
2023-08-29 01:35:00 +0800

    reorganize the test samples

--- pr_review/test/test_secret.py
+++ pr_review/test/test_secret.py
@@ -5,24 +5,53 @@ import hashlib
 import os
 
 # Replace these variables with your own values
-url = "http://127.0.0.1:8080/review_pr"
+url = "http://8.210.154.109:32766/review_pr"
 secret = os.environ.get("WEBHOOK_SECRET")
+
+# test sample #1
+# payload = {
+#     "action": "opened",
+#     "pull_request": {
+#         "title": "Add new status ImageError to fix #25",
+#         "body": "添加了一个新的状态ImageError，当镜像拉取失败时更新到该状态。",
+#         "number": 37
+#     },
+#     "repository": {
+#         "full_name": "OpenRHINO/RHINO-Operator"
+#     }
+# }
+
+# test sample #2
+# payload = {
+#     "action": "opened",
+#     "pull_request": {
+#         "title": "json added",
+#         "body": "convert logging info to json format",
+#         "number": 8
+#     },
+#     "repository": {
+#         "full_name": "ProgPanda/GPT-Assist"
+#     }
+# }
+
+# test sample #3
 payload = {
     "action": "opened",
-    "pull_request": {
-        "title": "Add new status ImageError to fix #25",
-        "body": "添加了一个新的状态ImageError，当镜像拉取失败时更新到该状态。",
-        "number": 37
-    },
-    "repository": {
-        "full_name": "OpenRHINO/RHINO-Operator"
-    }
+        "pull_request": {
+          "number": 2,
+          "title": "Sample Pull Request",
+          "body": "This is a sample pull request."
+        },
+        "repository": {
+          "full_name": "LI-Mingyu/cndev-tutorial"
+        }
 }
+
+
 headers = {
     "Content-Type": "application/json",
     "X-GitHub-Event": "pull_request"
 }
-
 
 def create_signature(payload, secret):
     hmac_obj = hmac.new(secret.encode(), json.dumps(
